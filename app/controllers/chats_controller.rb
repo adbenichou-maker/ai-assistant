63a62e48cdb591b5e58f976c95fcdcd36e3f43f2
Adam Benichou <adbenichou@gmail.com>
2025-11-25 16:37:05 +0100

    api adding

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,8 +2,6 @@ require 'ostruct'
 
 class ChatsController < ApplicationController
 
-
-
   def new
     @chat = Chat.new()
 
@@ -17,8 +15,43 @@ class ChatsController < ApplicationController
     }
   end
 
-  def create
+  before_action :set_chat, only: [:show]
+
+  SYSTEM_PROMPT = <<~PROMPT
+    You are an expert chef and cooking assistant.
+
+    The user will give you:
+    - ingredients available in their fridge
+    - dietary preferences
+    - cooking time constraints
+    - kitchen equipment available
+
+    Your job:
+    - Suggest 3 possible recipes
+    - Each recipe should be adapted to the ingredients they have
+    - If ingredients are missing, propose simple alternatives
+    - Keep instructions simple and beginner-friendly
+
+    Format:
+    - Use Markdown
+    - For each recipe:
+      - A title
+      - Short description
+      - List of ingredients needed
+      - Step-by-step instructions
+
+    Tone: friendly, helpful, educational.
+  PROMPT
+
+  def show
+    # A blank Message object for the new-message form
+    @message = Message.new
   end
 
+  private
+
+  def set_chat
+    @chat = current_user.chats.find(params[:id])
+  end
 
 end
