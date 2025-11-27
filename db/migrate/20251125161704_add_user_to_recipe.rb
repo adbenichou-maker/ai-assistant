ef5e9ca78c8e2a6e5b23c41e1358ea773b52da10
robertpniower <robert-pniower@gmx.de>
2025-11-27 11:32:35 +0100

    changed recipe table

--- db/migrate/20251125161704_add_user_to_recipe.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToRecipe < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :recipes, :user, foreign_key: true
-
-  end
-end
---
+++ db/migrate/20251127100028_change_recipies_table.rb
@@ -0,0 +1,23 @@
+class ChangeRecipiesTable < ActiveRecord::Migration[7.1]
+  def change
+
+    add_column :recipes, :content, :text
+
+    add_reference :recipes, :user, null: true, foreign_key: true
+
+    Recipe.reset_column_information
+
+    Recipe.where(content: nil).update_all(content: "## Placeholder Recipe Content\n\nPlease re-seed the database to generate full content.")
+
+    change_column_null :recipes, :content, false
+
+
+    remove_column :recipes, :title, :string if column_exists?(:recipes, :title)
+    remove_column :recipes, :cooking_time, :integer if column_exists?(:recipes, :cooking_time)
+    remove_column :recipes, :ingredients, :jsonb if column_exists?(:recipes, :ingredients)
+    remove_column :recipes, :steps, :jsonb if column_exists?(:recipes, :steps)
+    remove_column :recipes, :description, :text if column_exists?(:recipes, :description)
+
+
+  end
+end
