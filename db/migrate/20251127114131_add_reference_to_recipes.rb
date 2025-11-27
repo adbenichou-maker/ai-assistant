86eb2f2fea14b4761faaa84834789bb5a6bb8b44
Adam Benichou <adbenichou@gmail.com>
2025-11-27 15:12:28 +0100

    final

--- db/migrate/20251127114131_add_reference_to_recipes.rb
+++ db/migrate/20251127114131_add_reference_to_recipes.rb
@@ -1,6 +1,6 @@
 class AddReferenceToRecipes < ActiveRecord::Migration[7.1]
   def change
     add_column :recipes, :comment, :string
-    add_reference :recipes, :messages, foreign_key: true
+    add_reference :recipes, :message, foreign_key: true
   end
 end
