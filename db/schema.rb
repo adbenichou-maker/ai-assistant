a6721ddd93cdc2625e29e52c78784e12282c9180
Adam Benichou <adbenichou@gmail.com>
2025-11-27 18:12:43 +0100

    correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_27_165607) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_27_114131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,9 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_27_165607) do
     t.text "content", null: false
     t.string "comment"
     t.bigint "message_id"
-    t.bigint "user_id", null: false
     t.index ["message_id"], name: "index_recipes_on_message_id"
-    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,5 +58,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_27_165607) do
   add_foreign_key "chats", "users"
   add_foreign_key "messages", "chats"
   add_foreign_key "recipes", "messages"
-  add_foreign_key "recipes", "users"
 end
