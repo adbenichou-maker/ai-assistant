e95b274ef36d963a5ca75470e141de7cffe13c33
Adam Benichou <adbenichou@gmail.com>
2025-11-27 14:48:57 +0100

    advanced work on button

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,31 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all.order(created_at: :desc)
   end
 
+  def new
+    @message = Message.find(params[:message_id])
+    @recipe = Recipe.new(message: @message)
+  end
+
+  def create
+    @message = Message.find(params[:message_id])
+
+    # 2. Créer la Recipe en utilisant les Strong Parameters et l'association
+    @recipe = Recipe.new(recipe_params)
+    @recipe.message = @message
+
+    # Si vous voulez copier le contenu du message de l'IA dans la recette
+    # @recipe.content = @message.content
+
+    if @recipe.save
+      # 3. Rediriger vers la page du chat parent (@chat doit être récupéré via @message)
+      redirect_to chat_path(@message.chat), notice: "Recette sauvegardée !"
+    else
+      # Recharger le message pour la vue :new en cas d'erreur de validation
+      @message = Message.find(params[:message_id])
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -12,4 +37,13 @@ class RecipesController < ApplicationController
     @recipe.destroy
     redirect_to recipes_path, notice: "#{@recipe.title} was successfully deleted."
   end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :content, :notes)
+    # Ajoutez tous les attributs de Recipe qui peuvent être modifiés par le formulaire
+  end
+
 end
